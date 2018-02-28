685d328b344fdb8123d4b06d9adef40862fc3b37
Kevy Augusto <kevyaugusto@gmail.com>
2018-02-28 22:45:14 +1100

    solid principles solution

---
+++ SOLID_Principles/1-S/IUserWrong.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles.SRP
+{
+    interface IUserWrong
+    {
+        bool Login(string username, string password);
+        bool Register(string username, string password, string email);
+        void LogError(string error);
+        bool SendEmail(string emailContent);
+    }
+}
---
+++ SOLID_Principles/4-I/Wrong/HPPrinter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles._4_I
+{
+    class HPPrinter : Interfaces.IPrinterOperations
+    {
+        public bool FaxContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PhotoCopyContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PrintContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ScanContent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
